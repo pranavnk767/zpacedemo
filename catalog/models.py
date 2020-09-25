0ad131cab31f044bbd6da9aaa88f43f1702818f0
pranavnk767 <48652042+pranavnk767@users.noreply.github.com>
2020-09-25 10:17:56 +0530

    Revert "Revert "Django""
    This reverts commit 9c20522d8ee3ca291e65c1ae767d168d76c6720b.

---
+++ catalog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# coding: utf-8
+from sqlalchemy import CheckConstraint, Column, DateTime, Float, ForeignKey, Index, String, Table, Text, text
+from sqlalchemy.dialects.mysql import BIGINT, DATETIME, INTEGER, LONGTEXT, SMALLINT, TINYINT, TINYTEXT
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+metadata = Base.metadata
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+
+
