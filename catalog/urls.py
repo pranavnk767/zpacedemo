53395593a0472bcb6a49866cdc7e34f9b899d38d
pranavnk767 <48652042+pranavnk767@users.noreply.github.com>
2020-09-25 12:11:40 +0530

    Revert "demo"
    This reverts commit f082116c31ca189672b9360582398b8dfb03a547.

--- catalog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from django.conf.urls import include,url
-
-urlpatterns = [
-    url(r'^get_reviews',views.get_reviews,name='get_reviews'),
-    url(r'^register',views.saveRegister,name='register'),
-
-]
